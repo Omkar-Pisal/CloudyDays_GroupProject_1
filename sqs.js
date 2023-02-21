27c7982bfbcb5b12cabb85adee8a6a209d3f6aa4
Omkar Pisal <omkarpisal@Omkars-MacBook-Air.local>
2023-02-20 18:31:28 -0700

    Configured SQS

--- sqs.js
+++ sqs.js
@@ -1,36 +1,22 @@
-// Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
-// Set the region 
-AWS.config.update({region: 'us-east-1'});
+const AWS = require('aws-sdk');
+const sqs = new AWS.SQS({ region: 'us-east-1' }); // Replace region with your preferred region
+AWS.config.update({
+    region: 'us-east-1', // Replace with your preferred region
+    accessKeyId: process.env.AKIAXCTTWHYUEMGVVIO4,
+    secretAccessKey: process.env.zYK4x6MBzQEWC4ltXxynJlhm4J5b73wbvgUhqC1P,
+  });
 
-// Create an SQS service object
-var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+const message = {
+    MessageBody: 'Hello from Node.js!',
+  };
 
-var params = {
-  MessageAttributes: {
-    "Title": {
-      DataType: "String",
-      StringValue: "The Whistler"
-    },
-    "Author": {
-      DataType: "String",
-      StringValue: "John Grisham"
-    },
-    "WeeksOn": {
-      DataType: "Number",
-      StringValue: "6"
+  const queueUrl = 'https://sqs.us-east-1.amazonaws.com/486647676456/cloudydays-project1-sqs-input'; // Replace with your SQS queue URL
+
+  sqs.sendMessage({ QueueUrl: queueUrl, MessageBody: message.MessageBody }, (err, data) => {
+    if (err) {
+      console.log('Error sending message:', err);
+    } else {
+      console.log('Message sent:', data.MessageId);
     }
-  },
-  MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
-  // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
-  // MessageGroupId: "Group1",  // Required for FIFO queues
-  QueueUrl: "https://sqs.us-east-1.amazonaws.com/486647676456/cloudydays-project1-sqs-input"
-};
-
-sqs.sendMessage(params, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data.MessageId);
-  }
-});
+  });
+  
